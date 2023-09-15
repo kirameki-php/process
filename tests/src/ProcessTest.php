30b0648baef91a43b170045d2c9e1179ed9a989d
Taka Oyama <taka.oyama@gmail.com>
2023-09-15 09:36:47 +0900

    -1

--- tests/src/ProcessTest.php
+++ tests/src/ProcessTest.php
@@ -162,4 +162,132 @@ final class ProcessTest extends TestCase
         $this->assertTrue($result->failed());
         $this->assertTrue($result->timedOut());
     }
+
+    public function test_command_timed_out_disable_timeout(): void
+    {
+        $result = (new ProcessBuilder(['bash', 'exit.sh']))
+            ->timeout(0.01)->timeout(null)
+            ->inDirectory($this->getScriptsDir())
+            ->start()
+            ->wait();
+
+        $this->assertSame(['bash', 'exit.sh'], $result->info->executedCommand);
+        $this->assertSame(ExitCode::SUCCESS, $result->exitCode);
+        $this->assertNull($result->info->timeout);
+        $this->assertFalse($result->timedOut());
+    }
+
+    public function test_command_timed_out_change_signal(): void
+    {
+        $result = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '1']))
+            ->timeout(0.01, SIGINT, null)
+            ->exceptedExitCodes(ExitCode::TIMED_OUT)
+            ->inDirectory($this->getScriptsDir())
+            ->start()
+            ->wait();
+
+        $this->assertSame(['timeout', '--signal', (string) SIGINT, '0.01s', 'bash', 'exit.sh', '--sleep', '1'], $result->info->executedCommand);
+        $this->assertSame(ExitCode::TIMED_OUT, $result->exitCode);
+        $this->assertSame(SIGINT, $result->info->timeout?->signal);
+    }
+
+    public function test_command_timeout_command_error(): void
+    {
+        $result = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '1']))
+            ->timeout(-0.01)
+            ->exceptedExitCodes(ExitCode::TIMEOUT_COMMAND_FAILED)
+            ->inDirectory($this->getScriptsDir())
+            ->start()
+            ->wait();
+
+        $this->assertSame(ExitCode::TIMEOUT_COMMAND_FAILED, $result->exitCode);
+        $this->assertFalse($result->succeeded());
+        $this->assertTrue($result->failed());
+        $this->assertFalse($result->timedOut());
+    }
+
+    public function test_command_missing_script(): void
+    {
+        $this->expectExceptionMessage('Command not found. (code: 127, command: "noop.sh")');
+        $this->expectException(ProcessFailedException::class);
+
+        (new ProcessBuilder('noop.sh'))
+            ->start()
+            ->wait();
+    }
+
+    public function test_command_signal_on_running_process_as_success(): void
+    {
+        $process = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '1']))
+            ->inDirectory($this->getScriptsDir())
+            ->exceptedExitCodes(ExitCode::SIGHUP)
+            ->start();
+
+        $this->assertTrue($process->signal(SIGHUP));
+
+        $process->wait();
+
+        // try to signal again should return false.
+        $this->assertFalse($process->signal(SIGHUP));
+    }
+
+    public function test_command_sigint_on_running_process(): void
+    {
+        $this->expectExceptionMessage('Terminated by SIGINT (2). (code: 130, command: ["bash","exit.sh","--sleep","1"])');
+        $this->expectException(ProcessFailedException::class);
+
+        $process = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '1']))
+            ->inDirectory($this->getScriptsDir())
+            ->start();
+
+        $this->assertTrue($process->signal(SIGINT));
+
+        $process->wait();
+    }
+
+    public function test_command_signal_on_segfault_process(): void
+    {
+        $this->expectExceptionMessage('Terminated by SIGSEGV (11). (code: 139, command: ["bash","exit.sh","--sleep","5"])');
+        $this->expectException(ProcessFailedException::class);
+
+        $process = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '5']))
+            ->inDirectory($this->getScriptsDir())
+            ->start();
+
+        $this->assertTrue($process->signal(SIGSEGV));
+
+        $process->wait();
+    }
+
+    public function test_command_signal_on_terminated_process(): void
+    {
+        $this->expectExceptionMessage('Terminated by SIGKILL (9). (code: 137, command: ["bash","exit.sh","--sleep","5"])');
+        $this->expectException(ProcessFailedException::class);
+
+        $process = (new ProcessBuilder(['bash', 'exit.sh', '--sleep', '5']))
+            ->inDirectory($this->getScriptsDir())
+            ->start();
+
+        $this->assertTrue($process->signal(SIGKILL));
+
+        $process->wait();
+    }
+//
+//    public function test_command_signal_on_terminated_process_with_timeout(): void
+//    {
+//        $process = (new ProcessBuilder(['bash', 'trap-sigterm.sh']))
+//            ->exceptedExitCodes(ExitCode::SIGKILL)
+//            ->inDirectory($this->getScriptsDir())
+//            ->start();
+//
+//        // wait for the process to register trap.
+//        $output = $process->getIterator()->current();
+//
+//        $signaled = $process->terminate(0.01);
+//        $result = $process->wait();
+//
+//        $this->assertSame("trapped\n", $output);
+//        $this->assertTrue($signaled);
+//        $this->assertSame(ExitCode::SIGKILL, $result->exitCode);
+//    }
 }
